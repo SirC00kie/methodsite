8e70a694831ed1305420a9ae947ecd0c83b3101b
SirC00kie <kostomarov.2001@mail.ru>
2022-08-19 18:03:07 +0300

    Вынес в отдельный класс чтение таблиц из файла и расчёт компонентов A. В классе-наследнике реализуются методы

--- methods/methods_service.py
+++ methods/methods_service.py
@@ -1,123 +1,96 @@
 import numpy as np
-
 from .models import Matrix
-from .file_service import read_table_to_array, write_to_file
+from .file_service import read_table_to_array
 
-def get_matrix_parametrs():
-    d = dict();
-    d['stages'] = Matrix._meta.get_field('stages').value_from_object(Matrix.objects.first())
-    d['components'] = Matrix._meta.get_field('components').value_from_object(Matrix.objects.first())
-    d['experients'] = Matrix._meta.get_field('experients').value_from_object(Matrix.objects.first())
-    d['step'] = Matrix._meta.get_field('step').value_from_object(Matrix.objects.first())
-    return d
 
-def array_to_matrix_dictionary():
+class FunctionCalculate:
     array = read_table_to_array('methods/static/methods/json/tables.json')
-    matrixParam = get_matrix_parametrs()
+    matrixParam = dict()
+    table = dict()
 
-    d = dict();
-    d['stehCoef'] = array[:matrixParam['stages'] * matrixParam['components']]
-    d['pokazStep'] = array[matrixParam['stages'] * matrixParam['components']:matrixParam['stages'] * matrixParam['components'] * 2]
-    d['expDat'] = array[matrixParam['stages'] * matrixParam['components'] * 2: matrixParam['stages'] * matrixParam['components'] * 2 + matrixParam['experients'] * (matrixParam['components']+1)]
-    d['constSpeed'] = array[matrixParam['stages'] * matrixParam['components'] * 2 + matrixParam['experients'] * (matrixParam['components']+1):]
-    return d
+    matrixParam['stages'] = Matrix._meta.get_field('stages').value_from_object(Matrix.objects.first())
+    matrixParam['components'] = Matrix._meta.get_field('components').value_from_object(Matrix.objects.first())
+    matrixParam['experients'] = Matrix._meta.get_field('experients').value_from_object(Matrix.objects.first())
+    matrixParam['step'] = Matrix._meta.get_field('step').value_from_object(Matrix.objects.first())
 
-def system_diff_equation(y):
-    dictionary = array_to_matrix_dictionary()
-    matrixParam = get_matrix_parametrs()
+    table['stehCoef'] = array[:matrixParam['stages'] * matrixParam['components']]
+    table['pokazStep'] = array[
+                              matrixParam['stages'] * matrixParam['components']:matrixParam['stages'] * matrixParam[
+                                  'components'] * 2]
+    table['expDat'] = array[
+                           matrixParam['stages'] * matrixParam['components'] * 2: matrixParam['stages'] * matrixParam[
+                               'components'] * 2 + matrixParam['experients'] * (matrixParam['components'] + 1)]
+    table['constSpeed'] = array[
+                               matrixParam['stages'] * matrixParam['components'] * 2 + matrixParam['experients'] * (
+                                       matrixParam['components'] + 1):]
 
-    sum = np.zeros(matrixParam['components']);
-    A = np.zeros(matrixParam['components'])
+    def system_diff_equation(self, y):
 
-    for i in range(0,matrixParam['components']):
-        for j in range(0,matrixParam['stages']):
+        A = np.zeros(self.matrixParam['components'])
+        sumA = np.zeros(self.matrixParam['components'])
+        r = np.zeros(self.matrixParam['stages'])
+        sumR = np.zeros(self.matrixParam['stages'])
 
-            A[i] = dictionary['stehCoef'][i+(j*matrixParam['components'])] * dictionary['constSpeed'][j] * y[j] ** dictionary['pokazStep'][i+(j*matrixParam['components'])]
-            sum[i] += A[i]
+        for i in range(0, self.matrixParam['stages']):
+            sumR[i] = self.table['constSpeed'][i]
+            for j in range(0, self.matrixParam['components']):
+                if self.table['pokazStep'][j + (i * self.matrixParam['components'])] != 0:
+                    r[i] = y[j] ** self.table['pokazStep'][
+                        j + (i * self.matrixParam['components'])]
+                    sumR[i] *= r[i]
 
-    return sum
-
-def method_Euler():
-    dictionary = array_to_matrix_dictionary()
-    matrixParam = get_matrix_parametrs()
-
-    t = dictionary['expDat'][1]
-    h = matrixParam['step']
-    n = int(dictionary['expDat'][(matrixParam['experients'] * (matrixParam['components'] + 1) - (matrixParam['components'] + 1) )])
-    steps = int(n/h)
-
-    y = np.zeros((steps+1,matrixParam['components']+1))
-    x = 0
-
-    for i in range (steps+1):
-        y[i, 0] = x
-        x += h
-
-    for j in range(matrixParam['components']):
-        y[0,j+1] = dictionary['expDat'][j+1]
-
-    for i in range(0,steps,1):
-        for j in range(matrixParam['components']):
-            y[i+1,j+1] =  y[i,j+1] + h * system_diff_equation(y[i,1:])[j]
-
-    return y
-
-def method_RungeKuttaSecond():
-    dictionary = array_to_matrix_dictionary()
-    matrixParam = get_matrix_parametrs()
-
-    t = dictionary['expDat'][1]
-    h = matrixParam['step']
-    n = int(dictionary['expDat'][
-                (matrixParam['experients'] * (matrixParam['components'] + 1) - (matrixParam['components'] + 1))])
-    steps = int(n / h)
-
-    y = np.zeros((steps + 1, matrixParam['components'] + 1))
-    x = 0
-    for i in range(steps + 1):
-        y[i, 0] = x
-        x += h
-
-    for j in range(matrixParam['components']):
-        y[0, j + 1] = dictionary['expDat'][j + 1]
-
-    for i in range(0,steps,1):
-        for j in range(matrixParam['components']):
-            y[i+1,j+1] =  y[i,j+1] + (h / 2) * (system_diff_equation(y[i,1:])[j] + system_diff_equation(y[i,1:] + h * system_diff_equation(y[i,1:])[j])[j])
-
-    return y
-
-def method_RungeKuttaFourth():
-    dictionary = array_to_matrix_dictionary()
-    matrixParam = get_matrix_parametrs()
-
-    t = dictionary['expDat'][1]
-    h = matrixParam['step']
-    n = int(dictionary['expDat'][
-                (matrixParam['experients'] * (matrixParam['components'] + 1) - (matrixParam['components'] + 1))])
-    steps = int(n / h)
-
-    y = np.zeros((steps + 1, matrixParam['components'] + 1))
-    x = 0
-    for i in range(steps + 1):
-        y[i, 0] = x
-        x += h
-
-    for j in range(matrixParam['components']):
-        y[0, j + 1] = dictionary['expDat'][j + 1]
-
-    for i in range(0,steps,1):
-        for j in range(matrixParam['components']):
-            K1 = system_diff_equation(y[i,1:])[j]
-            K2 = system_diff_equation(y[i,1:] + h * K1 / 2)[j]
-            K3 = system_diff_equation(y[i,1:] + h * K2 / 2)[j]
-            K4 = system_diff_equation(y[i,1:] + h * K3)[j]
-            y[i+1,j+1] =  y[i,j+1] + (h / 6)  * (K1 + 2*K2 + 2*K3 + K4)
-    return y
+        for i in range(0, self.matrixParam['components']):
+            for j in range(0, self.matrixParam['stages']):
+                A[i] = self.table['stehCoef'][i + (j * self.matrixParam['components'])] * sumR[j]
+                sumA[i] += A[i]
+        return sumA
 
 
-def experients_table():
-    dictionary = array_to_matrix_dictionary()
-    return dictionary['expDat']
+class BaseMethods(FunctionCalculate):
+    def __init__(self):
+        self.t = self.table['expDat'][1]
+        self.h = self.matrixParam['step']
+        self.n = int(self.table['expDat'][
+                         (self.matrixParam['experients'] * (self.matrixParam['components'] + 1) - (
+                                 self.matrixParam['components'] + 1))])
+        self.steps = int(self.n / self.h)
 
+    def method(self):
+        x = 0
+        y = np.zeros((self.steps + 1, self.matrixParam['components'] + 1))
+        for i in range(self.steps + 1):
+            y[i, 0] = x
+            x += self.h
+
+        for j in range(self.matrixParam['components']):
+            y[0, j + 1] = self.table['expDat'][j + 1]
+        return y
+
+    def method_euler(self):
+        y = self.method()
+        for i in range(0, self.steps, 1):
+            for j in range(self.matrixParam['components']):
+                y[i + 1, j + 1] = y[i, j + 1] + self.h * self.system_diff_equation(y[i, 1:])[j]
+        return y
+
+    def method_runge_kutta_second(self):
+        y = self.method()
+        for i in range(0, self.steps, 1):
+            for j in range(self.matrixParam['components']):
+                y[i + 1, j + 1] = y[i, j + 1] + (self.h / 2) * \
+                                  (self.system_diff_equation(y[i, 1:])[j] +
+                                   self.system_diff_equation(
+                                       y[i, 1:] + self.h * self.system_diff_equation(y[i, 1:])[j])[j])
+        return y
+
+    def method_runge_kutta_fourth(self):
+        y = self.method()
+        for i in range(0, self.steps, 1):
+            for j in range(self.matrixParam['components']):
+                K1 = self.system_diff_equation(y[i, 1:])[j]
+                K2 = self.system_diff_equation(y[i, 1:] + self.h * K1 / 2)[j]
+                K3 = self.system_diff_equation(y[i, 1:] + self.h * K2 / 2)[j]
+                K4 = self.system_diff_equation(y[i, 1:] + self.h * K3)[j]
+                y[i + 1, j + 1] = y[i, j + 1] + (self.h / 6) * (K1 + 2 * K2 + 2 * K3 + K4)
+        return y
 
